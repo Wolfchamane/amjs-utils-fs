5b6a4b549988686d0f9cf8f3a147bcae7544cd31
Wolfchamane <arturo.mart.diaz@gmail.com>
2019-09-09 07:38:08 +0200

    0.1.0 -> 0.1.1

--- index.mjs
+++ index.mjs
@@ -1,4 +1,6 @@
+/* eslint-disable global-require */
 module.exports = {
     makeFolder  : require('./src/make-folder.mjs'),
     scanDir     : require('./src/scan-dir.mjs')
 };
+/* eslint-enable global-require */
